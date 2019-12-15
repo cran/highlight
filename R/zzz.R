74839db80ce3dba6b5ee790bc35ba163f7a6057a
Hadley Wickham <hadley@rstudio.com>
2019-12-15 15:10:05 +0000

    version 0.5.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,19 @@
 #' @importFrom grDevices rgb postscript dev.off pdf
 #' @importFrom tools vignetteEngine
-#' @useDynLib highlight
+#' @useDynLib highlight, .registration = TRUE
+#' @importFrom grDevices col2rgb colors
+#' @importFrom utils Rtangle RweaveChunkPrefix RweaveEvalWithOpt
+#'   RweaveLatexFinish RweaveLatexOptions RweaveLatexSetup RweaveTryStop
+#'   Sweave SweaveHooks getParseData tail
 NULL
 
 NAMESPACE <- environment()
 
 .onLoad <- function(libname, pkgname){
 	options( detective = simple_detective )
-	
+
 	vignetteEngine("highlight", weave = Hweave, tangle = Htangle,
-        pattern = "[.][hHrRsS]nw$", 
+        pattern = "[.][hHrRsS]nw$",
         package = "highlight")
 }
 
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 highlight
 =========
 
-[![Build Status](https://travis-ci.org/romainfrancois/highlight.png)](https://travis-ci.org/romainfrancois/highlight)
+<!-- badges: start -->
+![R build status](https://github.com/hadley/highlight/workflows/R-CMD-check/badge.svg)
+<!-- badges: end -->
 
 Syntax highlighter for R based on [highlight](http://www.andre-simon.de/doku/highlight/en/highlight.html)
 
